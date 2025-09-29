908014ddd280bc71fa2b311537dbfe0232f1268c
Julian Sanchez <julitosangame@gmail.com>
2025-09-29 18:56:40 -0400

    Implémentation de la seconde fonctionnalité

--- labo2.py
+++ labo2.py
@@ -1,7 +1,2 @@
-def salutations(nom: str):
-    return f"Bonjour {nom}."
-
-def age(annee_naissance: int):
-    from datetime import date
-    annee_actuelle = date.today().year
-    return f"Votre âge est {annee_actuelle - annee_naissance} ans." 
+def age(age):
+    return f"Votre âge est {age} ans." 
